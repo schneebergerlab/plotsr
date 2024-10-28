9fb81bfcc2b7e9d6e9ff85a24b5ec894bf5c4371
mnshgl0110 <mnshgl0110.gmail.com>
2024-10-28 15:11:51 +0100

    adding test data

---
+++ test/pytest.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+import unittest
+import os
+
+
+class TestHometools(unittest.TestCase):
+    def test_cli(self):
+        from subprocess import Popen, PIPE
+        p = Popen('plotsr -h'.split(), stdout=PIPE, stderr=PIPE)
+        out = p.communicate()
+        assert 'error' not in out[1].decode()
+
+    def test_pdf_out(self):
+        from subprocess import Popen, PIPE
+        import os
+        from glob import glob
+        output = 'plotsr.pdf'
+
+        try:
+            os.remove(output)
+        except OSError as e:
+            pass
+
+        command = f'plotsr --sr syri.out --genomes genomes.txt -o {output}'.split()
+        p = Popen(command, stdout=PIPE, stderr=PIPE)
+        out = p.communicate()
+
+        assert os.path.isfile(output)
+        os.remove(output)
+        for f in glob(f"*log"):
+            try:
+                os.remove(f)
+            except OSError as e:
+                if e.errno != 2:  # 2 is the error number when no such file or directory is present https://docs.python.org/2/library/errno.html
+                    raise
+
+    def test_png_out(self):
+        from subprocess import Popen, PIPE
+        import os
+        from glob import glob
+        output = 'plotsr.png'
+
+        try:
+            os.remove(output)
+        except OSError as e:
+            pass
+
+        command = f'plotsr --sr syri.out --genomes genomes.txt -o {output}'.split()
+        p = Popen(command, stdout=PIPE, stderr=PIPE)
+        out = p.communicate()
+
+        assert os.path.isfile(output)
+        os.remove(output)
+        for f in glob(f"*log"):
+            try:
+                os.remove(f)
+            except OSError as e:
+                if e.errno != 2:  # 2 is the error number when no such file or directory is present https://docs.python.org/2/library/errno.html
+                    raise
+
+
+if __name__ == '__main__':
+    os.chdir('test/test_data/')
+    unittest.main(verbosity=3)
+
+
