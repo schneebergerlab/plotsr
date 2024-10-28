6d39eb485222e0c85cf25847bdf3ff234af5ef2a
mnshgl0110 <mnshgl0110.gmail.com>
2024-10-28 15:09:30 +0100

    adding tests

--- test/test_plotsr.py
+++
@@ -1,5 +0,0 @@
-def test_cli():
-    from subprocess import Popen, PIPE
-    p = Popen('plotsr -h'.split(), stdout=PIPE, stderr=PIPE)
-    out = p.communicate()
-    assert 'error' not in out[1].decode()
